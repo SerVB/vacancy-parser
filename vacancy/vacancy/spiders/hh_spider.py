bd6ec0b55bb668d63208c09c20b0ce86585fbaf6
SerVB <servbul@yandex.ru>
2019-07-10 17:49:38 +0300

    Add splitting

--- vacancy/vacancy/spiders/hh_spider.py
+++ vacancy/vacancy/spiders/hh_spider.py
@@ -2,7 +2,7 @@
 
 from typing import Optional
 
-import scrapy
+from scrapy import Spider
 from scrapy.exporters import JsonItemExporter
 from scrapy.http.response import Response
 
@@ -25,25 +25,26 @@ def clean_up_place(s: Optional[str]) -> Optional[str]:
         return s.strip()
 
 
+RUSSIA_WITH_SALARY = "https://hh.ru/search/vacancy?area=113&clusters=true&enable_snippets=true&only_with_salary=true"
 # WITH_SALARY = "https://hh.ru/search/vacancy?clusters=true&enable_snippets=true&only_with_salary=true&from=cluster_compensation"
-SITE = "https://hh.ru/search/vacancy?clusters=true&employment={empl}&enable_snippets=true&industry={ind}&only_with_salary=true&specialization={spec}&from=cluster_professionalArea"
-
-divided_start_urls = list()
-
-ind_range = range(0, 100)
-spec_range = range(0, 100)
-employment = frozenset({
-    "volunteer",
-    "full",
-    "part",
-    "project",
-    "probation",
-})
-
-for ind in ind_range:
-    for spec in spec_range:
-        for empl in employment:
-            divided_start_urls.append(SITE.format(ind=ind, spec=spec, empl=empl))
+# SITE = "https://hh.ru/search/vacancy?clusters=true&employment={empl}&enable_snippets=true&industry={ind}&only_with_salary=true&specialization={spec}&from=cluster_professionalArea"
+#
+# divided_start_urls = list()
+#
+# ind_range = range(0, 100)
+# spec_range = range(0, 100)
+# employment = frozenset({
+#     "volunteer",
+#     "full",
+#     "part",
+#     "project",
+#     "probation",
+# })
+#
+# for ind in ind_range:
+#     for spec in spec_range:
+#         for empl in employment:
+#             divided_start_urls.append(SITE.format(ind=ind, spec=spec, empl=empl))
 
 
 class MyJsonItemExporter(JsonItemExporter):
@@ -51,31 +52,83 @@ class MyJsonItemExporter(JsonItemExporter):
         super(MyJsonItemExporter, self).__init__(file, ensure_ascii=False, **kwargs)
 
 
-class QuotesSpider(scrapy.Spider):
+class HhSpider(Spider):
+    MAX_PAGES = 100
+    VACANCY_PER_PAGE = 20
+
     name = "hh"
-    start_urls = divided_start_urls
+    start_urls = [RUSSIA_WITH_SALARY]
     custom_settings = {
         # "DEPTH_LIMIT": 5,
         "FEED_EXPORTERS": {"json": "vacancy.spiders.hh_spider.MyJsonItemExporter"}
     }
 
-    def parse(self, response: Response) -> None:
-        vacancy_count_str = response.css(".header.HH-SearchVacancyDropClusters-Header::text").get()
-        vacancy_count = int("".join(filter(lambda c: c.isdigit(), vacancy_count_str)))
+    scraped_pages = set()
 
-        max_pages = 100
-        vacancy_per_page = 20
-        if vacancy_count > max_pages * vacancy_per_page:
-            raise Exception(response.url, vacancy_count)
+    @staticmethod
+    def vacancy_count(response: Response) -> int:
+        vacancy_count_str = response.css(".header.HH-SearchVacancyDropClusters-Header::text").get()
+        return int("".join(filter(lambda c: c.isdigit(), vacancy_count_str)))
+
+    def split_clusters_group(self, response: Response, group_name: str, callback) -> None:
+        count = self.vacancy_count(response)
+        if count <= 0.95 * self.MAX_PAGES * self.VACANCY_PER_PAGE:
+            # Если уже малое число вакансий, не стоит делить дальше,
+            # т.к. вакансии не всегда отнесены к категории, и при разделении могут теряться
+            yield from self.parse_vacancy(response)
+            return
+
+        cluster_groups = response.css(".clusters-group")
+        for cluster_group in cluster_groups:
+            if clean_up_text(cluster_group.css(".clusters-group-title::text").get()) == group_name:
+                for item in cluster_group.css(
+                        "a.clusters-value:not(.clusters-value_selected):not(.clusters-list__item_more)"):
+                    yield response.follow(item, callback=callback)
+                return
+
+        yield from callback(response)
+        # raise Exception("Не найден кластер '%s' на %s" % (group_name, response.url))
+
+    def split_to_regions(self, response: Response) -> None:
+        yield from self.split_clusters_group(response, "Регион", self.split_to_field)
+
+    parse = split_to_regions
+
+    def split_to_field(self, response: Response) -> None:
+        yield from self.split_clusters_group(response, "Профобласть", self.split_to_specialty)
+
+    def split_to_specialty(self, response: Response) -> None:
+        yield from self.split_clusters_group(response, "Специализация", self.split_to_experience)
+
+    def split_to_experience(self, response: Response) -> None:
+        yield from self.split_clusters_group(response, "Опыт работы", self.split_to_industry)
+
+    def split_to_industry(self, response: Response) -> None:
+        yield from self.split_clusters_group(response, "Отрасль компании", self.split_to_schedule)
+
+    def split_to_schedule(self, response: Response) -> None:
+        yield from self.split_clusters_group(response, "График работы", self.split_to_sub_industry)
+
+    def split_to_sub_industry(self, response: Response) -> None:
+        yield from self.split_clusters_group(response, "Сфера компании", self.parse_vacancy)
+
+    def parse_vacancy(self, response: Response) -> None:
+        count = self.vacancy_count(response)
+        if count > self.MAX_PAGES * self.VACANCY_PER_PAGE:
+            raise Exception("Не могу спарсить большое число вакансий (%s) на %s" % (count, response.url))
 
         for vacancy in response.css(".vacancy-serp-item"):
-            yield {
-                "title": clean_up_text(vacancy.css(".resume-search-item__name a::text").get()),
-                "salary": clean_up_text(vacancy.css(".vacancy-serp-item__compensation::text").get()),
-                "firm": clean_up_text(vacancy.css(".vacancy-serp-item__meta-info a::text").get()),
-                "place": clean_up_place(vacancy.css("span.vacancy-serp-item__meta-info::text").get()),
-                "url": clean_up_text(vacancy.css(".resume-search-item__name a::attr(href)").get()),
-            }
+            url = clean_up_text(vacancy.css(".resume-search-item__name a::attr(href)").get())
+
+            if url not in self.scraped_pages:
+                self.scraped_pages.add(url)
+                yield {
+                    "title": clean_up_text(vacancy.css(".resume-search-item__name a::text").get()),
+                    "salary": clean_up_text(vacancy.css(".vacancy-serp-item__compensation::text").get()),
+                    "firm": clean_up_text(vacancy.css(".vacancy-serp-item__meta-info a::text").get()),
+                    "place": clean_up_place(vacancy.css("span.vacancy-serp-item__meta-info::text").get()),
+                    "url": url,
+                }
 
         for a in response.css("a.bloko-button.HH-Pager-Controls-Next.HH-Pager-Control"):
-            yield response.follow(a, callback=self.parse)
+            yield response.follow(a, callback=self.parse_vacancy)
