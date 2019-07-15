06fd055edc14063885cfc9c737792fc83fd7ff5f
SerVB <servbul@yandex.ru>
2019-07-16 01:00:15 +0300

    Add description scrapping

--- vacancy/vacancy/spiders/hh_spider.py
+++ vacancy/vacancy/spiders/hh_spider.py
@@ -42,7 +42,7 @@ class HhSpider(Spider):
     custom_settings = {
         # "DEPTH_LIMIT": 5,
         "FEED_EXPORTERS": {"json": "vacancy.spiders.hh_spider.MyJsonItemExporter"},
-        "ITEM_PIPELINES": {'vacancy.pipelines.DbPipeline': 300},
+        # "ITEM_PIPELINES": {'vacancy.pipelines.DbPipeline': 300},
     }
 
     scraped_pages = set()
@@ -57,7 +57,7 @@ class HhSpider(Spider):
         if count <= 0.95 * self.MAX_PAGES * self.VACANCY_PER_PAGE:
             # Если уже малое число вакансий, не стоит делить дальше,
             # т.к. вакансии не всегда отнесены к категории, и при разделении могут теряться
-            yield from self.parse_vacancy(response)
+            yield from self.parse_vacancy_page(response)
             return
 
         cluster_groups = response.css(".clusters-group")
@@ -92,25 +92,31 @@ class HhSpider(Spider):
         yield from self.split_clusters_group(response, "График работы", self.split_to_sub_industry)
 
     def split_to_sub_industry(self, response: Response) -> None:
-        yield from self.split_clusters_group(response, "Сфера компании", self.parse_vacancy)
+        yield from self.split_clusters_group(response, "Сфера компании", self.parse_vacancy_page)
 
-    def parse_vacancy(self, response: Response) -> None:
+    def parse_vacancy_page(self, response: Response) -> None:
         count = self.vacancy_count(response)
         if count > self.MAX_PAGES * self.VACANCY_PER_PAGE:
             raise Exception("Не могу спарсить большое число вакансий (%s) на %s" % (count, response.url))
 
         for vacancy in response.css(".vacancy-serp-item"):
-            url = clean_up_text(vacancy.css(".resume-search-item__name a::attr(href)").get())
-
-            if url not in self.scraped_pages:
-                self.scraped_pages.add(url)
-                yield {
-                    "title": clean_up_text(vacancy.css(".resume-search-item__name a::text").get()),
-                    "salary": clean_up_text(vacancy.css(".vacancy-serp-item__compensation::text").get()),
-                    "firm": clean_up_text(vacancy.css(".vacancy-serp-item__meta-info a::text").get()),
-                    "place": clean_up_place(vacancy.css("span.vacancy-serp-item__meta-info::text").get()),
-                    "url": url,
-                }
+            for url in vacancy.css(".resume-search-item__name a"):
+                follow = response.follow(url, callback=self.parse_vacancy)
+                follow.meta["place"] = clean_up_place(vacancy.css("span.vacancy-serp-item__meta-info::text").get())
+                yield follow
 
         for a in response.css("a.bloko-button.HH-Pager-Controls-Next.HH-Pager-Control"):
-            yield response.follow(a, callback=self.parse_vacancy)
+            yield response.follow(a, callback=self.parse_vacancy_page)
+
+    def parse_vacancy(self, response: Response) -> None:
+        description = clean_up_text("\n".join(response.css(".vacancy-section::text, "
+                                                           ".vacancy-section *::text").extract()))
+
+        yield {
+            "title": clean_up_text(response.css("h1.header::text").get()),
+            "salary": clean_up_text(response.css("p.vacancy-salary::text").get()),
+            "firm": clean_up_text(response.css(".vacancy-company-name span::text").get()),
+            "place": response.meta["place"],
+            "url": response.url,
+            "description": description
+        }
