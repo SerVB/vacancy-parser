155feae24e59e8af60bcee6ef1ca37f9adb008d0
SerVB <servbul@yandex.ru>
2019-07-25 01:41:40 +0300

    Enhance splitting

--- vacancy/vacancy/spiders/hh_spider.py
+++ vacancy/vacancy/spiders/hh_spider.py
@@ -1,8 +1,9 @@
 # -*- coding: utf-8 -*-
 
-from typing import Optional
+import logging
+from typing import Optional, List
 
-from scrapy import Spider
+from scrapy import Spider, Request
 from scrapy.exporters import JsonItemExporter
 from scrapy.http.response import Response
 
@@ -26,6 +27,8 @@ def clean_up_place(s: Optional[str]) -> Optional[str]:
 
 
 RUSSIA_WITH_SALARY = "https://hh.ru/search/vacancy?area=113&clusters=true&enable_snippets=true&only_with_salary=true"
+MOSCOW_WITH_SALARY = "https://hh.ru/search/vacancy?clusters=true&enable_snippets=true&only_with_salary=true&area=1&" \
+                     "from=cluster_area&showClusters=true"
 
 
 class MyJsonItemExporter(JsonItemExporter):
@@ -38,65 +41,93 @@ class HhSpider(Spider):
     VACANCY_PER_PAGE = 20
 
     name = "hh"
-    start_urls = [RUSSIA_WITH_SALARY]
     custom_settings = {
         # "DEPTH_LIMIT": 5,
         "FEED_EXPORTERS": {"json": "vacancy.spiders.hh_spider.MyJsonItemExporter"},
         # "ITEM_PIPELINES": {'vacancy.pipelines.DbPipeline': 300},
     }
 
-    @staticmethod
-    def vacancy_count(response: Response) -> int:
-        vacancy_count_str = response.css(".header.HH-SearchVacancyDropClusters-Header::text").get()
-        return int("".join(filter(lambda c: c.isdigit(), vacancy_count_str)))
+    max_count = 0
+    scrapped = 0
 
-    def split_clusters_group(self, response: Response, group_name: str, callback) -> None:
-        count = self.vacancy_count(response)
+    def closed(self, reason):
+        logging.warning("Примерно потеряно вакансий: %s" % (self.max_count - self.scrapped))
+
+    def start_requests(self) -> List[Request]:
+        return [
+            Request(
+                url=RUSSIA_WITH_SALARY,
+                callback=self.split_clusters_group,
+                meta={"split_by": frozenset((
+                    "Регион",
+                    "Профобласть",
+                    "Специализация",
+                    "Опыт работы",
+                    "Отрасль компании",
+                    "График работы",
+                    "Сфера компании",
+                    "Тип занятости",
+                ))}
+            ),
+            # Request(
+            #     url=MOSCOW_WITH_SALARY,
+            #     callback=self.split_clusters_group,
+            #     meta={"split_by": frozenset((
+            #         "Профобласть",
+            #         "Специализация",
+            #         "Опыт работы",
+            #         "Отрасль компании",
+            #         "График работы",
+            #         "Сфера компании",
+            #         "Тип занятости",
+            #     ))}
+            # ),
+        ]
+
+    def vacancy_count(self, response: Response) -> int:
+        vacancy_count_str = response.css(".header.HH-SearchVacancyDropClusters-Header::text").get()
+        vacancy_count_int = int("".join(filter(lambda c: c.isdigit(), vacancy_count_str)))
+        self.max_count = max(self.max_count, vacancy_count_int)
+        return vacancy_count_int
+
+    def split_clusters_group(self, response: Response) -> None:
+        count = self.vacancy_count(response)  # количество вакансий вверху страницы
         if count <= 0.95 * self.MAX_PAGES * self.VACANCY_PER_PAGE:
             # Если уже малое число вакансий, не стоит делить дальше,
             # т.к. вакансии не всегда отнесены к категории, и при разделении могут теряться
             yield from self.parse_vacancy_page(response)
             return
 
+        # сначала пробуем разбивать точно:
         cluster_groups = response.css(".clusters-group")
         for cluster_group in cluster_groups:
-            if clean_up_text(cluster_group.css(".clusters-group-title::text").get()) == group_name:
+            split = clean_up_text(cluster_group.css(".clusters-group-title::text").get())
+            if split in response.meta["split_by"]:
+                sum_count = 0  # сумма вакансий внутри списка
+                for item in cluster_group.css("a.clusters-value .clusters-value__count::text"):
+                    sum_count += int("".join(filter(lambda c: c.isdigit(), item.get())))
+
+                if sum_count == count:  # можно разбить точно?
+                    for item in cluster_group.css(
+                            "a.clusters-value:not(.clusters-value_selected):not(.clusters-list__item_more)"):
+                        follow = response.follow(item, callback=self.split_clusters_group)
+                        follow.meta["split_by"] = frozenset(response.meta["split_by"] - frozenset({split}))
+                        yield follow
+                    return
+
+        # если не получилось, пробуем разбить хоть как-то:
+        for cluster_group in cluster_groups:
+            split = clean_up_text(cluster_group.css(".clusters-group-title::text").get())
+            if split in response.meta["split_by"]:
                 for item in cluster_group.css(
                         "a.clusters-value:not(.clusters-value_selected):not(.clusters-list__item_more)"):
-                    yield response.follow(item, callback=callback)
-                return
+                    follow = response.follow(item, callback=self.split_clusters_group)
+                    follow.meta["split_by"] = frozenset(response.meta["split_by"] - frozenset({split}))
+                    yield follow
 
-        yield from callback(response)
-        # raise Exception("Не найден кластер '%s' на %s" % (group_name, response.url))
-
-    def split_to_regions(self, response: Response) -> None:
-        yield from self.split_clusters_group(response, "Регион", self.split_to_field)
-
-    parse = split_to_regions
-
-    def split_to_field(self, response: Response) -> None:
-        yield from self.split_clusters_group(response, "Профобласть", self.split_to_specialty)
-
-    def split_to_specialty(self, response: Response) -> None:
-        yield from self.split_clusters_group(response, "Специализация", self.split_to_experience)
-
-    def split_to_experience(self, response: Response) -> None:
-        yield from self.split_clusters_group(response, "Опыт работы", self.split_to_industry)
-
-    def split_to_industry(self, response: Response) -> None:
-        yield from self.split_clusters_group(response, "Отрасль компании", self.split_to_schedule)
-
-    def split_to_schedule(self, response: Response) -> None:
-        yield from self.split_clusters_group(response, "График работы", self.split_to_sub_industry)
-
-    def split_to_sub_industry(self, response: Response) -> None:
-        yield from self.split_clusters_group(response, "Сфера компании", self.parse_vacancy_page)
+        yield from self.parse_vacancy_page(response)
 
     def parse_vacancy_page(self, response: Response) -> None:
-        count = self.vacancy_count(response)
-        if count > self.MAX_PAGES * self.VACANCY_PER_PAGE:
-            raise Exception("Не могу спарсить большое число вакансий (%s) на %s" % (count, response.url))
-
         for vacancy in response.css(".vacancy-serp-item"):
             for url in vacancy.css(".resume-search-item__name a"):
                 follow = response.follow(url, callback=self.parse_vacancy)
@@ -110,8 +141,10 @@ class HhSpider(Spider):
         description = clean_up_text("\n".join(response.css(".vacancy-section")[0]
                                               .css("*:not(style):not(script)::text").extract()))
 
+        self.scrapped += 1
+
         yield {
-            "title": clean_up_text(response.css("h1.header::text").get()),
+            "title": clean_up_text(response.css(".vacancy-title h1.header::text").get()),
             "salary": clean_up_text(response.css("p.vacancy-salary::text").get()),
             "firm": clean_up_text(response.css(".vacancy-company-name span::text").get()),
             "place": response.meta["place"],
